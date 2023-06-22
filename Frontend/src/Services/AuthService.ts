6024ea2a036d2271a8551515b0ab73c769e690a3
Michal Resnick <michalf3160@gmail.com>
2023-06-22 06:42:53 +0300

    verify admin and loggedin

--- Frontend/src/Services/AuthService.ts
+++ Frontend/src/Services/AuthService.ts
@@ -3,6 +3,7 @@ import CredentialsModel from "../Models/CredentialsModel";
 import UserModel from "../Models/UserModel";
 import { AuthActionType, authStore } from "../Redux/AuthState";
 import appConfig from "../Utils/Config";
+import jwtDecode from "jwt-decode";
 
 class AuthService {
 
@@ -37,7 +38,31 @@ class AuthService {
         authStore.dispatch({ type: AuthActionType.Logout });
     }
 
+    public async isEmailAddressTaken(email:string):Promise<boolean>{
+        const response=await axios.get<boolean>(appConfig.authUrl+email)
+       return response.data //the email is already exists.
+
+    }
+
+    public isAdmin(user: UserModel = null): boolean {
+        if (!user) {
+            user = authStore.getState().user;
+            if (!user) return false;
+        }
+        return user.role === "Admin";
+    }
+
+    // Check if a valid token exists;
+    public isLoggedIn(): boolean {
+        if (authStore.getState().token === null) return false;
+        const container: { exp: number } = jwtDecode(authStore.getState().token);
+        const now = new Date();
+        //token.exp is in seconds, while Date.getTime is in milliseconds
+        return container.exp * 1000 > now.getTime();
+    }
+
 }
+
 
 const authService = new AuthService();
 
--- Frontend/src/Utils/Config.ts
+++ Frontend/src/Utils/Config.ts
@@ -3,6 +3,7 @@ class Config {
     public followersUrl = "http://localhost:3001/api/followers/";
     public registerUrl = "http://localhost:3001/api/auth/register";
     public loginUrl = "http://localhost:3001/api/auth/login";
+    public authUrl = "http://localhost:3001/api/auth/";
 }
 
 const appConfig = new Config(); // Singleton
