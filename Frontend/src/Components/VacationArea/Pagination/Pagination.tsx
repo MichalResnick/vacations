e55f552551825256d5d6b1f3120255f41a2fac10
Michal Resnick <michalf3160@gmail.com>
2023-06-28 05:25:01 +0300

    pagination

---
+++ Frontend/src/Components/VacationArea/Pagination/Pagination.tsx
@@ -0,0 +1,33 @@
+import "./Pagination.css"
+
+interface PaginationProps {
+  totalPosts: number;
+  postsPerPage: number;
+  setCurrentPage: (page: number) => void;
+  currentPage: number;
+}
+
+function Pagination({ totalPosts, postsPerPage, setCurrentPage, currentPage }: PaginationProps): JSX.Element {
+  let pages = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pages.push(i);
+  }
+
+  return (
+    <div className='pagination'>
+      {pages.map((page, index) => (
+        <button
+          key={index}
+          onClick={() => setCurrentPage(page)}
+          className={page === currentPage ? 'active' : ''}
+        >
+          {page}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Pagination;
+
--- Frontend/src/Components/VacationArea/VacationsList/VacationsList.tsx
+++ Frontend/src/Components/VacationArea/VacationsList/VacationsList.tsx
@@ -5,12 +5,24 @@ import vacationsService from "../../../Services/VacationsService";
 import Spinner from "../../SharedArea/Spinner/Spinner";
 import VacationCard from "../VacationCard/VacationCard";
 import { vacationsStore } from "../../../Redux/VacationsState";
+import Pagination from "../Pagination/Pagination";
+import { FavoriteBorder, Favorite } from "@mui/icons-material";
+import { Checkbox } from "@mui/material";
+import authService from "../../../Services/AuthService";
+
 
 function VacationsList(): JSX.Element {
 
   const [vacations, setVacations] = useState<VacationModel[]>([])
+  const [checked, setChecked] = useState<boolean>(false);
   const[currentPage,setCurrentPage]=useState(1)
   const[postsPerPage,setPostsPerPage]=useState(8)
+  const lastIndex=currentPage * postsPerPage
+  const firstIndex=lastIndex-postsPerPage
+  const currentVacation=vacations.slice(firstIndex,lastIndex)
+
+
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
   useEffect(() => {
     vacationsService.getAllVacations()
@@ -28,9 +40,23 @@ function VacationsList(): JSX.Element {
 
   }, [])
 
-  const lastIndex=currentPage * postsPerPage
-  const firstIndex=lastIndex-postsPerPage
-  const currentVacation=vacations.slice(firstIndex,lastIndex)
+  async function filterMyVacation(args:any){
+    try {
+      if(args.target.checked){
+        vacationsService.getMyVacations()
+        .then(myvacations=>{
+          setVacations(myvacations),
+          setChecked(true)})
+
+        .catch()
+      }
+      
+    } catch (error) {
+      alert      
+    }
+  }
+
+
 
   return (
     <div className="VacationList">
@@ -39,8 +65,17 @@ function VacationsList(): JSX.Element {
 
       {currentVacation.length === 0 && <Spinner />}
       {currentVacation.map(v => <VacationCard key={v.vacationId} vacation={v} />)}
+
+      {!authService.isAdmin() && <>
+                <Checkbox {...label} icon={<FavoriteBorder />} color="secondary" checkedIcon={<Favorite />} checked={checked} onChange={filterMyVacation} className="CheckBoxFilter" />
+                <p className="MyVacations">My Vacations</p>
+            </>}
+
+
+     <Pagination totalPosts={vacations.length} postsPerPage={postsPerPage} setCurrentPage={setCurrentPage } currentPage={currentPage}/>
     </div>
   );
+
 }
 
 export default VacationsList;
