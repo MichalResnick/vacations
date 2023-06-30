2915aad675b1e125b755522694c9cb9933b23833
Michal Resnick <michalf3160@gmail.com>
2023-06-30 06:16:07 +0300

    csv

--- Frontend/src/Components/AdminArea/VacationCharts/VacationCharts.tsx
+++ Frontend/src/Components/AdminArea/VacationCharts/VacationCharts.tsx
@@ -3,9 +3,9 @@ import VacationModel from '../../../Models/VacationModel';
 import vacationsService from '../../../Services/VacationsService';
 import { Bar } from 'react-chartjs-2';
 import { Chart, registerables } from 'chart.js';
+import { CSVLink } from 'react-csv';
+// Import CSVLink component from react-csv
 Chart.register(...registerables);
-
-
 
 function VacationsChart(): JSX.Element {
   const [vacations, setVacations] = useState<VacationModel[]>([]);
@@ -15,7 +15,8 @@ function VacationsChart(): JSX.Element {
       .then((fetchedVacations) => {
         setVacations(fetchedVacations);
       })
-      .catch((err:any) => {console.error(err);
+      .catch((err:any) => {
+        console.error(err);
         // Handle error, e.g., show an error message
       });
   }, []);
@@ -36,10 +37,19 @@ function VacationsChart(): JSX.Element {
     ],
   };
 
+  // Define CSV data
+  const csvData = vacations.map((vacation) => ({
+    Target: vacation.target,
+    FollowersCount: vacation.followersCount,
+  }));
+
   return (
     <div style={{ margin: '20px', padding: '20px', backgroundColor: '#f2f2f2' }}>
       <h2>Vacations Chart</h2>
       <Bar data={chartData} />
+      <CSVLink data={csvData} filename={'vacations.csv'}>
+        Download CSV
+      </CSVLink>
     </div>
   );
 }
